7c754fb1c2703a68a962861ca23452cf5ebd6a49
Aaron Yih <aaron.yih@ucla.edu>
2015-04-13 15:53:21 -0700

    comments added to code

--- 2/bill.cpp
+++ 2/bill.cpp
@@ -14,25 +14,29 @@ using namespace std;
 
 int main()
 {
+    //prompt user for initial meter reading
     cout<<"Initial meter reading: ";
     double readingOne;
     cin>>readingOne;
     
+    //prompt user for final meter reading
     cout<< "Final meter reading: ";
     double readingTwo;
     cin>>readingTwo;
     cin.ignore(10000,'\n');
     
+    //propt user for their name
     cout << "Customer name: ";
     string name;
     getline(cin,name);
     
+    //prompt user for the month
     cout << "Month number (1=Jan, 2=Feb, etc.): ";
     int month;
     cin>>month;
     
     double bill;
-    int HCF;
+    double HCF;
     HCF = readingTwo-readingOne; //finding the change of the meter over that given month
     
     // checking if high or low season
@@ -60,36 +64,41 @@ int main()
     }
     cout<<fixed<<showpoint;
 
-    cout<<"Initial meter reading: "<<readingOne<<endl;
-    cout<<"Final meter reading: "<<readingTwo<<endl;
-    cout<<"Customer name: "<<name<<endl;
-    cout<<"Month number (1=Jan, 2=Feb, etc.): "<<month<<endl;
     cout<<"---"<<endl;
     
+    
+    //checks to make sure that the first meter reading is positive
     if(readingOne < 0)
     {
-        cout << "The initial meter reading must be nonnegative.";
+        cout << "The initial meter reading must be nonnegative."<<endl;
         return(0);
     }
     
+    //checks to make sure that the second meter reading is larger than the first
     else if(readingTwo < readingOne)
     {
-        cout<<"The final reading must be at least as large as the initial reading.";
+        cout<<"The final reading must be at least as large as the initial reading."<<endl;
         return(0);
     }
+    
+    //checks if the user's name is empty
     else if(name.length() == 0)
     {
-        cout<<"You must enter a customer name.";
+        cout<<"You must enter a customer name."<<endl;
         return(0);
     }
+    
+    // checks to see if the month is a valid month
     else if(month < 1 || month >12)
     {
-        cout<<"The month number must be in the range 1 through 12.";
+        cout<<"The month number must be in the range 1 through 12."<<endl;
         return(0);
     }
+    
+    //if none of these errors occurs, print the bill
     else
     {
-        cout<< "The bill for " << name << " is $" <<setprecision(2)<< bill << '\n';
+        cout<< "The bill for " << name << " is $" <<setprecision(2)<< bill << '\n'<<endl;
     }
     
 }
