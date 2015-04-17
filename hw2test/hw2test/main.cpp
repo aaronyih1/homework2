f6e4777464ff95a450d909f82b594b90c91292f7
Aaron Yih <aaron.yih@ucla.edu>
2015-04-17 16:06:44 -0700

    some changes

---
+++ hw2test/hw2test/main.cpp
@@ -0,0 +1,37 @@
+//
+//  main.cpp
+//  hw2test
+//
+//  Created by Aaron Yih on 4/17/15.
+//  Copyright (c) 2015 aaronyih. All rights reserved.
+//
+
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int len;
+    
+    cout << "Enter a number: ";
+    cin >> len;
+    int i = 0;
+    if(len < 0)
+        len = len*(-1);
+    else
+        len = len;
+    
+    do
+    {
+        int j = i+1;
+        while(j < len)
+        {
+            cout << " ";
+            j++;
+        }
+        cout << "#" << endl;
+        i++;
+    }
+    while(i < len);
+    
+}
